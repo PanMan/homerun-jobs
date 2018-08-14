4014d6c2dfd9800a1317f2748ade31e131dc0bbf
PanMan <git@panman.nl>
2018-08-14 19:57:18 +0200

    disable not needed func

--- src/twigextensions/HomerunJobsTwigExtension.php
+++ src/twigextensions/HomerunJobsTwigExtension.php
@@ -68,7 +68,7 @@ class HomerunJobsTwigExtension extends \Twig_Extension
     {
         return [
             // new \Twig_SimpleFunction('someFunction', [$this, 'someInternalFunction']),
-            new \Twig_SimpleFunction('homerunJobs', [$this, 'getHomerunJobs','getCompanyData']),
+            new \Twig_SimpleFunction('homerunJobs', [$this, 'getHomerunJobs']),
         ];
     }
 
@@ -119,35 +119,15 @@ class HomerunJobsTwigExtension extends \Twig_Extension
      *
      * @return object
      */
+    /*
     public function getCompanyData($text = null)
     {
-        //Get homerun slugs from craft and use that to fire request to homerun and return
-
+        //Get all companies. Disabled for now
         $entries = Entry::find()
             ->section('companies')
-            // ->select('companyHomerunSlug') //Somehow doesnt work
             ->all();
 
-        // $slugs=[];
-        // foreach ($entries as $item){
-        //     if ($item->companyHomerunSlug){
-        //         $slugs[]=$item->companyHomerunSlug;
-        //     }
-        // }
-
-        // $baseurl="https://api.homerun.co/v1/tq-jobs?companies=".join(',',$slugs);
-        // $curl = curl_init();
-        // // Set some options - we are passing in a useragent too here
-        // curl_setopt_array($curl, array(
-        //     CURLOPT_RETURNTRANSFER => 1,
-        //     CURLOPT_URL => $baseurl,
-        //     CURLOPT_USERAGENT => 'Craft 3 Homerun Plugin',
-        //     CURLOPT_USERPWD => getenv('HOMERUN_APIKEY').':'
-        // ));
-        // // Send the request & save response to $resp
-        // $resp = curl_exec($curl);
-        // // Close request to clear up some resources
-        // curl_close($curl);
         return json_encode($entries);
     }
+    */
 }
